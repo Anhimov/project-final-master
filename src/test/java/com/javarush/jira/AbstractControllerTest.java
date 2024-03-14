030be2c922569c911e9c264213aef9b6203b8f35
Anhimov <anhimov.d@gmail.com>
2024-03-14 16:13:28 +0300

    FileUtil-upload-method-refactoring

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -1,15 +1,18 @@
 package com.javarush.jira;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications
 @Sql(scripts = {"classpath:db/changelog.sql", "classpath:data.sql"}, config = @SqlConfig(encoding = "UTF-8"))
+@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications-testing-with-mock-environment
 public abstract class AbstractControllerTest extends BaseTests {
